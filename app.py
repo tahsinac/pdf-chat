4bbd0d42ee9b6cfc722920668c8e16832559ed86
Tahsin Ashraf <tahsin.ac@live.com>
2024-03-11 20:39:17 -0400

    created get_pdf_text

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 def main ():
     load_dotenv()
-    
+
     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
     st.header("Chat with multiple PDFs")
@@ -12,6 +21,20 @@ def main ():
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
 
 if __name__ == "__main__":
     main()
